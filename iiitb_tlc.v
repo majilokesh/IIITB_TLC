54f525f47d7f73b4233654e4668b43677eb08b44
majilokesh <72696170+majilokesh@users.noreply.github.com>
2022-07-27 10:04:49 +0530

    Update iiitb_tlc.v

--- iiitb_tlc.v
+++ iiitb_tlc.v
@@ -19,7 +19,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 // Verilog project: Verilog code for traffic light controller
-module traffic_light(light_highway, light_farm, C, clk, rst_n);
+module iiitb_tlc(light_highway, light_farm, C, clk, rst_n);
 	parameter HGRE_FRED=2'b00, // Highway green and farm red
    HYEL_FRED = 2'b01,// Highway yellow and farm red
    HRED_FGRE=2'b10,// Highway red and farm green
