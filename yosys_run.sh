9f5b19a791f83f3c16b6207fd17e3b1275b2b50d
majilokesh <72696170+majilokesh@users.noreply.github.com>
2022-08-04 16:43:07 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -7,9 +7,13 @@ synth -top iiitb_tlc
 
 # mapping to mycells.lib
 dfflibmap -liberty /home/lokesh/iiitb_tlc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/lokesh/iiitb_tlc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-clean
+proc 
+opt
+abc -liberty /home/lokesh/iiitb_tlc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
+proc
 flatten
+clean
+
 # write synthesized design
-write_verilog no-attr iiitb_tlc_synth.v
+write_verilog -noattr iiitb_tlc_synth.v
 stat
