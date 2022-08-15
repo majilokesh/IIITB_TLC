495c6fd72d00ae1223b6c71974c71214df2ed0b7
majilokesh <72696170+majilokesh@users.noreply.github.com>
2022-08-15 22:32:32 +0530

    Update iiitb_tlc_tb.v

--- iiitb_tlc_tb.v
+++ iiitb_tlc_tb.v
@@ -21,96 +21,73 @@
 // Additional Comments:
 // 
 ////////////////////////////////////////////////////////////////////////////////
-// 1. Preprocessor Directives
-`define DELAY 1
-// 2. Include Statements
-//`include "counter_define.h"
+
 module iiitb_tlc_tb;
-// 3. Parameter definitions
-parameter ENDTIME  = 40000;
-// 4. DUT Input regs
-//integer count, count1, a;
-reg clk;
-reg rst_n;
-reg sensor;
-wire [2:0] light_farm;
-// 5. DUT Output wires
-wire [2:0] light_highway;
-// 6. DUT Instantiation
-iiitb_tlc test(light_highway, light_farm, sensor, clk, rst_n);
 
-// 7. Initial Conditions
-initial
- begin
- clk = 1'b0;
- rst_n = 1'b0;
- sensor = 1'b0;
- // count = 0;
-//// count1=0;
-// a=0;
- end
- initial
-  begin
-   $dumpfile("tlc_out.vcd");
-   $dumpvars(0, iiitb_tlc_tb);
-  end
- // 8. Generating Test Vectors
-initial
- begin
- main;
- end
-task main;
- fork
- clock_gen;
- reset_gen;
- operation_flow;
- debug_output;
- endsimulation;
- join
-endtask
-task clock_gen;
- begin
- forever #`DELAY clk = !clk;
- end
- endtask
+	parameter ENDTIME  = 40000;
 
-task reset_gen;
- begin
- rst_n = 0;
- # 20
- rst_n = 1;
- end
-endtask
-task operation_flow;
- begin
-  sensor = 0;
- # 600
- sensor = 1;
- # 1200
- sensor = 0;
- # 1200
- sensor = 1;
- end
-endtask
-// 9. Debug output
-task debug_output;
- begin
- $display("----------------------------------------------");
-        $display("------------------     -----------------------");
- $display("----------- SIMULATION RESULT ----------------");
-  $display("--------------             -------------------");
- $display("----------------         ---------------------");
- $display("----------------------------------------------");
- $monitor("TIME = %d, reset = %b, sensor = %b, light of highway = %h, light of farm road = %h",$time,rst_n ,sensor,light_highway,light_farm );
- end
-endtask
-//10. Determines the simulation limit
-task endsimulation;
- begin
- #ENDTIME
- $display("-------------- THE SIMUALTION END ------------");
- $finish;
- end
-endtask
+	reg clk;
+	reg rst_n;
+	reg sensor;
+	wire [2:0] light_farm;
+	wire [2:0] light_highway;
+
+//DUT Instantiation
+	iiitb_tlc test(light_highway, light_farm, sensor, clk, rst_n);
+
+//Initial Conditions
+	initial
+		 begin
+		 clk = 1'b0;
+		 rst_n = 1'b0;
+		 sensor = 1'b0;
+
+		 end
+	initial
+		  begin
+		   $dumpfile("tlc_out.vcd");
+		   $dumpvars(0, iiitb_tlc_tb);
+	  	  end
+//Generating Test Vectors
+
+	always #1 clk = !clk;
+	initial
+		begin
+			rst_n = 0;
+			sensor = 0;
+			#60
+			sensor = 1;
+			# 140
+			rst_n = 1;
+			#120
+			sensor = 0;
+			#120
+			sensor = 1;
+			#120 sensor = 0;
+			#120 sensor = 1;
+			#120 sensor = 0;
+			#120 sensor = 1;
+			#1000
+			sensor = 0;
+		end
+//Debug output
+
+	initial
+		begin
+			$display("----------------------------------------------");
+			$display("------------------     -----------------------");
+	 		$display("----------- SIMULATION RESULT ----------------");
+	  		$display("--------------             -------------------");
+			$display("----------------         ---------------------");
+			$display("----------------------------------------------");
+			$monitor("TIME = %d, reset = %b, sensor = %b, light of highway = %h, light of farm road = %h",$time,rst_n ,sensor,light_highway,light_farm );
+		end
+
+	initial
+		begin
+			 #ENDTIME
+			 $display("-------------- THE SIMUALTION END ------------");
+			 $finish;
+	 	end
     
 endmodule
